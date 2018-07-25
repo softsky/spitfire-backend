3f40c83ea8a68439301903a36b8bc4022b4309b5
nazarlitvin <n.litvin41@gmail.com>
2018-07-25 16:12:13 +0300

    remove selenium

--- playground/loginWithWebDriver.js
+++ playground/loginWithWebDriver.js
@@ -9,28 +9,6 @@ async function waitForTitle() {
   // const page = await browser.newPage();
   // await page.goto('https://www.nike.com/jp/launch/');
   // const title = await page.title();
-  
-  const chromeCapabilities = webdriver.Capabilities.chrome();
-  chromeCapabilities.set('chromeOptions', {args: ['--headless']});
-
-  const driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .withCapabilities(chromeCapabilities)
-    .build();
-
-  try {
-    // Navigate to google.com, enter a search.
-    await driver.get('https://www.nike.com/jp/launch/')
-    console.log('loaded');
-
-    // Wait until a title contais SNKRS
-    await driver.wait(until.titleContains('SNKRS'), 10000);
-    console.log('title contains SNKRS');
-    
-    driver.quit();
-  } catch(error) {
-    console.log(error);
-  }
 }
 
 
