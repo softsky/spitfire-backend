8dd9e27f3202e5741dc3823b10c1931ea0501d18
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-07-26 12:32:06 +0000

    .gitignore now ignores playground/success/* and playground/failure/*

--- playground/loginWithPuppeteer.js
+++ playground/loginWithPuppeteer.js
@@ -6,10 +6,6 @@ const puppeteer = require('puppeteer')
 , async = require('async')
 , fs = require('fs');
 
-
-//const username = 'adidasjpreport+9661@gmail.com';
-//const password = 'Teamjordan123';
-
 const paPairs = []
 , accounts = [];
 
@@ -24,89 +20,96 @@ function shuffle(a) {
     }
     return a;
 }
+
 async function runWithProxy() {
+	const uuid = require('uuid')
+	, fname = uuid()
+	, requests = [];
+
 	const {proxy, account} = paPairs.pop()
 	, oldProxyUrl = `http://${proxy.user}:${proxy.pwd}@${proxy.ip}:${proxy.port}`
 	, proxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-	
+	//require('events').EventEmitter.prototype.setMaxListeners(50);
 	console.info('Connect using proxy:', proxy, ' and account ', account);
-	await puppeteer.launch({
+	const browser = await puppeteer.launch({
 		headless: true,
+		slowMo: 50,
 		args: [
 			`--proxy-server=${proxyUrl}`,
-		]})
-		.then(async browser => {
-			const page = await browser.newPage();
+		]});
+	const page = await browser.newPage();
 
-			async function setInputValue(sel, val) {
-				await page.evaluate((data) => {
-					return document.querySelector(data.sel).value = data.val;
-				}, {sel, val});
-			}
+	async function setInputValue(sel, val) {
+		await page.evaluate((data) => {
+			return document.querySelector(data.sel).value = data.val;
+		}, {sel, val});
+	}
 
-			try {
-				//await page.authenticate({username: proxy.user, password: proxy.pwd});
-				//const page = await browser.targets()[1].page();
-				await page.setRequestInterception(true);
-				page.on('request', request => {
-					//console.log(request.url());
-					request.continue();
-				});
-				
-				page.setViewport({width:960,height:768});
-				
-				await page.goto('https://www.nike.com/jp/launch/', {
-					/* waitUntil: ['domcontentloaded', 'networkidle0'] */});
-				
+	async function saveLog(path){
+		fs.mkdirSync(`${path}`);
+		await page.screenshot({path: `${path}/screenshot.png`});
+		const html = await page.$eval('body', e => e.outerHTML);
+		await fs.createWriteStream(`${path}/page.html`)
+			.write(html);
+		// await fs.createWriteStream(`${path}/cookies.json`)
+		// 	.write(JSON.stringify(page.cookies()));
+		await fs.createWriteStream(`${path}/requests.json`)
+			.write(JSON.stringify(requests));
+	}
 
-				// puppeteer solution, not clicking on Login button somehow
-				await page.click('button[aria-label="メニュー"]');
-				console.log('Looking for button'); 
-				// const buttons = await page.$x("//*[contains(text(), 'Nike+')]");
-				// if (buttons.length > 0) {
-				// 	console.log('Button:', buttons[0].textContent);
-				// 	await buttons[0].click();
-				// } else {
-				// 	throw new Error("Login button not found");
-				// }
+	try {
+		// await page.setRequestInterception(true);
+		// page.on('request', request => {
+		// 	//console.log(request.url());
 
-				// solution 2 - does not work
-				// (await page.$$eval('button', b => b
-				//                    .filter(b => b.textContent.indexOf('Nike+') > -1)
-				//                   ))[0].click()
+		// 	requests.push({
+		// 		url: request.url(),
+		// 		method: request.method(),
+		// 		postData: request.postData(),
+		// 		headers: request.headers(),
+		// 		response: request.response()
+		// 	});
+		// 	request.continue();
+		// });
 
-				// solution 3 does not work
-				// const e = await page.mainFrame().$('button[data-qa = "join-login-button"]');
-				// if(!e)
-				// 	throw new Error('Click button not found');
-				// await e.click();
 
-				await page.evaluate(async () => {
-					document.querySelector('#root > div > div > div.main-layout.no-scroll > nav > ul > li:nth-child(1) > button').click();
-				});
-				
+		await page.goto('https://www.nike.com/jp/launch/', {
+			waitUntil: ['domcontentloaded', 'networkidle0']});
 
-				console.log('Clicked');
-				await page.waitForSelector('input[type="email"]');
-				await setInputValue('input[type="email"]', account.username);
-				await setInputValue('input[type="password"]', account.password);
-				await page.click('input[type="button"]');
+		page.setViewport({width:375,height:812});
+		page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92');
 
-				console.info('succesfully logged in');
-				await page.evaluate(async () => {
-					
-				});
-			} catch(e){
-				browser.close();
-				console.error(e);
-				// restoring proxy, account in our stack in case of exception
-				console.info(`Storing {${proxy}, ${account}} back to Stack`);
-				paPairs.push({proxy, account}); 
-			}
-		})
+		await page.click('button[data-qa = "mobile-nav-menu-button"]');
+
+		console.log('[---] clicked');
+		await page.evaluate(async () => {
+			document.querySelector('#root > div > div > div.main-layout.no-scroll > nav > ul > li:nth-child(1) > button').click();
+		});
+
+
+		console.log('clicked');
+		await page.waitForSelector('input[type="email"]');
+		console.log('setting values');
+		await setInputValue('input[type="email"]', account.username),
+		await setInputValue('input[type="password"]', account.password);
+
+		await page.click('input[type="button"]');
+		console.info('succesfully logged in. queue size:' + paPairs.length);
+
+		await saveLog(`playground/success/${fname}`);
+	} catch(e){
+		console.error(e);
+		// restoring proxy, account in our stack in case of exception
+		console.info(`Storing {${proxy}, ${account}} back to Stack`);
+		paPairs.push({proxy, account});
+
+		await saveLog(`playground/failure/${fname}`);
+
+	}
+	browser.close();
 }
 (async () => {
-	const numSeries = 10;
+	const numSeries = 20;
 	const pipeline = util.promisify(stream.pipeline);
 	
 	const accountPath = __dirname + '/accounts.csv'
@@ -153,6 +156,6 @@ async function runWithProxy() {
 			.then(cb)
 			.catch(console.error.bind(console));
 	});
-	await async.parallelLimit(async_queue, numSeries);
+	async.parallelLimit(async_queue, numSeries, () => process.exit(0));
 })();
 
