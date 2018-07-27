65aab6cfb2298b8cf629a496f57e7e3e470fa757
nazarlitvin <n.litvin41@gmail.com>
2018-07-27 18:40:59 +0300

    fix keygen api url & make code cleaner

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ class App {
   constructor(window) {
     this.window = window;
 
-    const homePath = app.getPath('home')
+    const homePath = app.getPath('home');
     this.settingsFilePath = path.join(homePath, '.spitfire.settings.json');
     
     this.initIpcs();
+  }
+
+  initIpcs() {
+    ipc.on('FETCH_NEW_RELEASES', this.fetchReleases.bind(this));
+    ipc.on('VALIDATE_LICENSE_KEY', this.validateLicense.bind(this));
   }
 
   getLicense() {
@@ -21,33 +26,44 @@ class App {
     return licenseKey;
   }
 
-  initIpcs() {
-    ipc.on('FETCH_NEW_RELEASES', async ({ sender }) => {
-      try {
-        const payload = await fetchNewReleases();
-        sender.send('NEW_RELEASES_SUCCESS', payload);
-      } catch (error) {
-        sender.send('NEW_RELEASES_FAILURE', error);
-      }
-    });
+  handleLicenseValid(sender, licenseKey) {
+    const settings = { licenseKey };        
+    fs.writeFileSync(this.settingsFilePath, JSON.stringify(settings));
+    this.window.setSize(1410, 690);        
+    sender.send('VALIDATE_LICENSE_KEY_SUCCESS', licenseKey);
+  }
 
-    ipc.on('VALIDATE_LICENSE_KEY', async ({ sender }, licenseKey) => {
-      try {
-        if (!licenseKey) {
-          licenseKey = await this.getLicense();
-        }
-        
-        const payload = await checkLicense(licenseKey);
-        const settings = { licenseKey };
-        
-        fs.writeFileSync(this.settingsFilePath, JSON.stringify(settings));
-        this.window.setSize(1410, 690);
-        
-        sender.send('VALIDATE_LICENSE_KEY_SUCCESS', payload);
-      } catch (error) {
-        sender.send('VALIDATE_LICENSE_KEY_FAILURE', error);
+  handleLicenseInvalid(sender) {
+    sender.send('VALIDATE_LICENSE_KEY_FAILURE', error);
+  }
+
+  async fetchReleases({ sender }) {
+    try {
+      const payload = await fetchNewReleases();
+      sender.send('NEW_RELEASES_SUCCESS', payload);
+    } catch (error) {
+      sender.send('NEW_RELEASES_FAILURE', error);
+    }
+  }
+
+  async validateLicense({ sender }, licenseKey) {
+    try {
+      if (!licenseKey) {
+        licenseKey = this.getLicense();
       }
-    });
+      
+      const response = await checkLicense(licenseKey);
+      const { valid, detail } = response.data.meta;
+
+      if (valid) {
+        this.handleLicenseValid(sender, licenseKey);
+      } else {
+        this.handleLicenseInvalid(sender, detail);
+      }
+
+    } catch (error) {
+      this.handleLicenseInvalid(sender, error);
+    }
   }
 }
 
--- services/checkLicense.js
+++ services/checkLicense.js
@@ -3,7 +3,7 @@ const axios = require('axios-https-proxy-fix');
 
 const KEYGEN_ACCOUNT_ID = '67608f49-9d1a-4ca3-b84f-04ae8e2aeab7';
 const KEYGEN_PRODUCT_ID = 'dd8ca258-98b2-4779-a186-16eedcc2b37a';
-const URL = 'https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses/actions/validate-key';
+const URL = `https://api.keygen.sh/v1/accounts/${KEYGEN_ACCOUNT_ID}/licenses/actions/validate-key`;
 
 module.exports = function checkLicense(key) {
   const headers = {
