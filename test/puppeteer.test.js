e19e6ef95cf7722eebc6b0def8386450fe6a7ed8
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-07-27 19:09:20 +0300

    jest upgraded, --env=node added for backend testing, tests fixed

--- test/puppeteer.test.js
+++ test/puppeteer.test.js
@@ -1,23 +1,39 @@
-var mocha = require('mocha')
-, chai = require('chai')
+const chai = require('chai')
 , chaiAsPromised = require('chai-as-promised')
 , assert = chai.assert
 , expect = chai.expect
-, should = chai.should()
-, _ = require('lodash');
+, should = chai.should();
 
 chai.use(chaiAsPromised);
 
-const Promise = require('bluebird');
+const Promise = require('bluebird')
+, puppeteer = require('puppeteer');
 
-describe('', () => {
-    before((done) => {
-        seneca.ready(done);
+describe('Puppeteer testing suite', () => {
+    let browser;
+
+    beforeAll(async () => {
+    	browser = await puppeteer.launch({headnless: true});
     });
-    describe('', () => {
-	it('', (done) => {
+
+    afterAll(async () => await browser.close());
+
+    describe('Puppeteer', () => {
+	it('Should succesfully run puppeteer on execution environment', async () => {
+	    const browser = await require('puppeteer').launch({headnless: true})
+	    const page = await browser.newPage();
+	    await browser.close();
+
+	    return browser;
+        });
+    })
+
+    describe('Scenario',  () => {
+	it('Should properly load scenario', async () => {
 	    // const login = require('./scenarios/login')
 	    // , logout = require('./scenarios/login')
-	    // runScenario([login, logout])
-        });
-    })})
+	    // runScenario([login, logout])	    
+	});
+    })
+})
+ 
