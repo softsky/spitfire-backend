2a2ad716b7aa9b7b14f23d46048319931ad3fe5c
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-08-04 02:16:46 +0300

    adding config that fix bug with jest running headless puppeteer

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// jest.config.js
+const {defaults} = require('jest-config');
+module.exports = {
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'js'],
+    testURL: 'http://localhost'
+};
