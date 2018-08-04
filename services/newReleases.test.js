5cb2a3fa23c92fba4f257ce44974cec8e6a8c261
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-08-04 11:13:10 +0300

    slightly modified newReleases and test

---
+++ services/newReleases.test.js
@@ -0,0 +1,22 @@
+const chai = require('chai')
+, chaiAsPromised = require('chai-as-promised')
+, assert = chai.assert
+, expect = chai.expect
+, should = chai.should()
+, main = require('./newReleases');
+
+chai.use(chaiAsPromised);
+
+
+describe('Puppeteer testing suite', () => {
+    describe('Scenario',  () => {
+	it('Should properly load scenario', async () => {
+	    const releases = await main();
+
+	    expect(releases).to.be.an('array');
+            expect(releases).to.have.property('length').not.eql(0);
+	    console.log(releases);
+
+	}, 10000);
+    })
+})
