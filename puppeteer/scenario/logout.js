d7d23b544f7b1cdbef4884f92dd7ffd813cf429b
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-08-08 15:48:23 +0300

    logout fixedc

--- puppeteer/scenario/logout.js
+++ puppeteer/scenario/logout.js
@@ -1,10 +1,10 @@
 module.exports = async (options, page) => {
-    const btn = await page.waitForSelector('#root > div > div > div.main-layout > div > header > div.d-sm-h.d-lg-b > section > ul > li.member-nav-item.d-sm-ib.va-sm-m > div > div > button > div > span > font > font', {timeout: 2000, visible:true});
+    const btn = await page.waitForSelector('#root > div > div > div.main-layout > div > header > div.d-lg-h.d-sm-b > section > div.prl4-sm.prl7-lg.va-sm-m.ta-sm-r > button', {timeout: 20000, visible:true});
     
-    await page.click('#root > div > div > div.main-layout > div > header > div.d-sm-h.d-lg-b > section > ul > li.member-nav-item.d-sm-ib.va-sm-m > div > div > button > div > span > font > font');
+    await page.click('#root > div > div > div.main-layout > div > header > div.d-lg-h.d-sm-b > section > div.prl4-sm.prl7-lg.va-sm-m.ta-sm-r > button');
 
-    await page.waitForSelector('#root > div > div > div.main-layout > nav > ul > li:nth-child(4) > button', {timeout: 2000, visible:true});
-    await page.click('#root > div > div > div.main-layout > nav > ul > li:nth-child(4) > buttonx');
+    await page.waitForSelector('#root > div > div > div.main-layout > nav > ul > li:nth-child(4) > button', {timeout: 20000, visible:true});
+    await page.click('#root > div > div > div.main-layout > nav > ul > li:nth-child(4) > button');
     //sleep(5);
     //await page.waitForNavigation({waitUntil: ['domcontentloaded', 'networkidle2']});
 }
