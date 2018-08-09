fb8b3c08e80d5123df5b37f82611d01bfc15c4c5
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-08-09 19:00:07 +0300

    partially fixed purchase, runScenarios need more work with async

--- puppeteer/runScenario.js
+++ puppeteer/runScenario.js
@@ -1,11 +1,14 @@
-const proxyChain = require('proxy-chain')
-  , csv = require('csv')
-  , async = require('async');
+const proxyChain = require('proxy-chain');
+const csv = require('csv');
+const async = require('async');
+const puppeteer = require('puppeteer');
+const logger = require('../utils/logger');
 
-const paPairs = []
-  , accounts = []
-  , proxies = []
-  , numSeries = 1;
+const paPairs = [];
+const accounts = [];
+const proxies = [];
+const numSeries = 1;
+
 
 /**
  * Shuffles array in place. ES6 version
@@ -20,128 +23,139 @@ function shuffle(a) {
 }
 
 const initProxies = async () => {
-    const stream = require('stream')
-      , util = require('util')
-      , fs = require('fs');
 
-    const pipeline = util.promisify(stream.pipeline);
-
-    const accountPath = __dirname + '/../accounts.csv'
-      , proxyPath = __dirname + '/../proxies.csv'
-      , accountStream = fs.createReadStream(accountPath)
-      , proxyStream = fs.createReadStream(proxyPath);
-
-    // FIXME:
-    await pipeline(proxyStream,
-		   csv.parse({ delimiter: ':' }),
-		   csv.transform((record) => {
-		       const proxy = {
-			   ip: record[0],
-			   port: record[1],
-			   user: record[2],
-			   pwd: record[3],
-		       };
-		       proxies.push(proxy);
-		   }));
-    shuffle(proxies);
-
-    await pipeline(accountStream,
-		   csv.parse({ delimiter: ':' }),
-		   csv.transform((record) => {
-		       proxies.unshift(proxies.pop());
-		       const username = record[0],
-		             password = record[1],
-	                     account = { username, password },
-		             proxy = proxies[0];
-		       accounts.push(account);
-
-		       paPairs.push({ proxy, account });
-		   }));
-
-    // shuffling our array of pairs
-    shuffle(paPairs);
-
+  // if already filled, return
+  if(paPairs.length){
     return paPairs;
-  }, runScenario = async (scenarioFunction) => {
-    const paPairs = await initProxies(); // we should make sure our proxies are loaded
-    const uuid = require('uuid');
-    const puppeteer = require('puppeteer');
-
-    let proxy, account, oldProxyUrl, proxyUrl;
-    if (paPairs.length) {
-      const obj = { proxy, account } = paPairs.pop();
-      oldProxyUrl = `http://${proxy.user}:${proxy.pwd}@${proxy.ip}:${proxy.port}`;
-      proxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
-    }
-    //require('events').EventEmitter.prototype.setMaxListeners(50);
-    console.info('Connect using proxy:', proxy, ' and account ', account);
-
-
-    const args = [
-      '--disable-setuid-sandbox',
-      '--no-sandbox',
-      '--disable-web-security',
-      '--disable-infobars',
-      '--disable-accelerated-2d-canvas',
-      '--disable-gpu',
-      '--ignore-certificate-errors',
-      '--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92"',
-    ];
-    if(proxyUrl){
-      args.push(`--proxy-server=${proxyUrl}`);
-    }
-    try {
-      console.log('ProxyURL', proxyUrl, 'args', args);
-      const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 100,
-        ignoreHTTPSErrors: true,
-        args,
-      });
-      console.log('Running');
-      const pages = await browser.pages();
-      const page = pages[0];
-      await page.addScriptTag({ 'url': 'https://code.jquery.com/jquery-3.3.1.js' }); //easier to debug selectors, will remove later
-      await page.setViewport({ width: 375, height: 812 });
-
-      await page.goto('https://www.nike.com/jp/launch/', {
-        waitUntil: ['domcontentloaded', 'networkidle0'],
-      });
-
-      if (Array.isArray(scenarioFunction)) {
-        scenarioFunction.forEach(async f => {
-          await f({ proxy, account }, page);
-        });
-      } else {
-        await scenarioFunction({ proxy, account }, page);
-      }
-      await page.waitFor(6000);
-
-    } catch (e) {
-      console.log(e);
-      // restoring proxy, account in our stack in case of exception
-      console.info(`Storing ${JSON.stringify({ proxy, account })} back to Stack`);
-      paPairs.push({ account, proxy });
-    } finally {
-      //browser.close(); //will change once order functionality is done
-    }
-  };
-
-(async () => {
-  // logging
-  console.info('Running tasks:', paPairs.length, ' in series ', numSeries);
-
-  while (paPairs.length > 0) {
-    console.log('New round');
-    //filling asyncqueue with tasks
-    //const account = {username: 'okanonike@gmail.com', password: 'Okano123'}
-    const async_queue = Array(paPairs.length).fill(() => runScenario([require('./scenario/login'), require('./scenario/purchase')]));
-    await new Promise((resolve, reject) => {
-      async.parallelLimit(async_queue, numSeries, results => resolve(results));
-    });
-    console.log('All done');
-    process.exit(0);
   }
-})();
 
-module.exports = runScenario;
+  const stream = require('stream');
+  const util = require('util');
+  const fs = require('fs');
+
+  const pipeline = util.promisify(stream.pipeline);
+
+  const accountPath = __dirname + '/../accounts.csv';
+  const proxyPath = __dirname + '/../proxies.csv';
+  const accountStream = fs.createReadStream(accountPath);
+  const proxyStream = fs.createReadStream(proxyPath);
+
+  // FIXME:
+  await pipeline(
+    proxyStream,
+    csv.parse({ delimiter: ':' }),
+    csv.transform((record) => {
+      const proxy = {
+        ip: record[0],
+        port: record[1],
+        user: record[2],
+        pwd: record[3],
+      };
+      proxies.push(proxy);
+    }));
+
+  await pipeline(
+    accountStream,
+    csv.parse({ delimiter: ':' }),
+    csv.transform((record) => {
+      proxies.unshift(proxies.pop());
+      const username = record[0];
+      const password = record[1];
+      const account = { username, password };
+      const proxy = proxies[0];
+      accounts.push(account);
+
+      paPairs.push({ proxy, account });
+    }));
+
+  // shuffling our array of pairs
+  // shuffle(paPairs); // TODO: uncomment that
+
+  return paPairs;
+};
+
+const runScenarios = async (scenarioFunctionArray) => {
+  const paPairs = await initProxies(); // we should make sure our proxies are loaded
+
+  const { proxy, account } = paPairs.pop();
+  const oldProxyUrl = `http://${proxy.user}:${proxy.pwd}@${proxy.ip}:${proxy.port}`;
+  const proxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl);
+  //require('events').EventEmitter.prototype.setMaxListeners(50);
+  logger.info(`Connect using proxy:${JSON.stringify(proxy)} and account ${JSON.stringify(account)}`);
+
+
+  const args = [
+    '--disable-setuid-sandbox',
+    '--no-sandbox',
+    '--disable-web-security',
+    '--disable-infobars',
+    '--disable-accelerated-2d-canvas',
+    '--disable-gpu',
+    '--ignore-certificate-errors',
+    '--user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92"',
+  ];
+  if(proxyUrl){
+    args.push(`--proxy-server=${proxyUrl}`);
+  }
+  //args.push('https://www.nike.com/jp/launch/');
+  let browser;
+  try {
+    browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      slowMo: 50,
+      ignoreHTTPSErrors: true,
+      args,
+    });
+    logger.debug('Running');
+    const page = (await browser.pages())[0];
+    await page.goto('https://www.nike.com/jp/launch', {
+      waitUntil: ['domcontentloaded', 'networkidle2'],
+    });
+    await page.setViewport({ width: 375, height: 812 });
+
+    if (Array.isArray(scenarioFunctionArray) == false)
+      throw new Error('scenarioFunctionArray should be an array');
+    // executing one by one
+    // while(scenarioFunctionArray.length > 0){
+    //   const f = scenarioFunctionArray.pop();
+    //   logger.info(`Executing scenario function ${f}`);
+    //   await f({ account, proxy }, { page, logger });
+    // }
+    const series =
+          Array(scenarioFunctionArray.length)
+          .fill(() => scenarioFunctionArray.pop()({ account, proxy }, { page, logger }));
+
+    async.everySeries(series);
+
+    logger.debug('Scenarios execution finished');
+
+    browser.close();
+  } catch (e) {
+    // restoring proxy, account in our stack in case of exception
+    logger.info(`Storing ${JSON.stringify({ proxy, account })} back to Stack`);
+    paPairs.push({ account, proxy });
+    throw e;
+  } finally {
+    //will change once order functionality is done
+  }
+};
+
+// (async () => {
+//   // logging
+//   logger.info('Running tasks:', paPairs.length, ' in series ', numSeries);
+
+//   while (paPairs.length > 0) {
+//     logger.debug('New round');
+//     //filling asyncqueue with tasks
+//     //const account = {username: 'okanonike@gmail.com', password: 'Okano123'}
+//     const async_queue = Array(paPairs.length).fill(() => runScenarios([require('./scenario/login'), require('./scenario/purchase')], paPairs.pop()));
+//     await new Promise((resolve) => {
+//       async.parallelLimit(async_queue, numSeries, results => resolve(results));
+//     });
+//     logger.debug('All done');
+//     process.exit(0);
+//   }
+// })();
+
+module.exports = { runScenarios, initProxies };
--- puppeteer/runScenario.test.js
+++ puppeteer/runScenario.test.js
@@ -1,29 +1,46 @@
-const chai = require('chai')
-  , chaiAsPromised = require('chai-as-promised')
-  , assert = chai.assert
-  , expect = chai.expect
-  , should = chai.should();
+/*eslint no-unused-vars: "off"*/
+const chai = require('chai');
+const expect = chai.expect;
 
-chai.use(chaiAsPromised);
-
+/*eslint no-undef: "off"*/
 describe('Puppeteer testing suite', () => {
-  describe('Scenario',  () => {
-    it('Should properly load and execute set of scenarios', async (done) => {
-	const injector = async (options, page) => {
-            expect(page).is.an('object');
-            expect(options).is.an('object');
-            expect(options).has.property('account').is.an('object');
-            expect(options).has.property('proxy').is.an('object');
-            //expect(options).has.property('order').is.an('object');
-        }
-        , runScenario = require('./runScenario');
-        const login = require('./scenario/login'),
-              logout = require('./scenario/logout'),
-              purchase = require('./scenario/purchase');
+  const { runScenarios, initProxies } = require('./runScenario');
 
-	runScenario([login, injector,  purchase, logout]);
-        done();
-    }, 30000);
+  describe('Single Scenario',  () => {
+    it('Should properly load and execute single login scenario', async function(done) {
+      const injector = async (options, env) => {
+        const { page, logger } = env;
+        expect(options).is.an('object');
+        expect(env).is.an('object');
+        expect(options).has.property('account').is.an('object');
+        expect(options).has.property('proxy').is.an('object');
+        expect(env).has.property('page').is.an('object');
+        expect(env).has.property('logger').is.an('object');
+      };
+      const login = require('./scenario/login');
+
+      runScenarios([login, injector]);
+      done();
+    }, 30000)
+
   });
-});
 
+  // describe('Multiple Scenarios',  () => {
+  //   it('Should properly load and execute set of scenarios', async (done) => {
+  //     const injector = async (options) => {
+  //       expect(options).is.an('object');
+  //       expect(options).has.property('account').is.an('object');
+  //       expect(options).has.property('proxy').is.an('object');
+  //       //expect(options).has.property('page').is.an('object');
+  //       //expect(options).has.property('order').is.an('object');
+  //     };
+  //     const login = require('./scenario/login');
+  //     const logout = require('./scenario/logout');
+  //     const purchase = require('./scenario/purchase');
+
+  //     runScenarios([login, injector,  purchase, logout], (await initProxies()).pop());
+  //     done();
+  //   }, 30000);
+  // });
+
+});
